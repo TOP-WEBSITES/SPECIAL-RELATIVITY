d47fae82e3e10d300b44b7545a71163c7d73fce2
Mayukh Das <mayukh4321@gmail.com>
2022-10-08 19:12:25 +0800

    added paradox article

--- src/views/chapters/ParadoxesChapter.vue
+++ src/views/chapters/ParadoxesChapter.vue
@@ -2,95 +2,137 @@
   <div class='chapter-container'>
     <h1>Paradoxes</h1>
     <h1 style="font-size: 32px; font-weight: 200"><em>Lee Jia Jie</em></h1><br>
-    <p>Turpis mi consectetur a nunc lacus pellentesque aliquet. Et feugiat amet auctor amet ac. Mi integer duis risus
-      ut nunc nunc id fringilla. Vitae elit lacus, faucibus egestas venenatis. Est vitae ipsum eget cursus nulla
-      sagittis. Auctor at risus lectus senectus risus enim. Lectus adipiscing nibh eros, dapibus et leo enim massa.
-      Mauris nunc massa adipiscing suspendisse semper eget sed donec. Elementum morbi enim diam dolor, sit sapien.
-      Fringilla massa, nunc scelerisque turpis.
-    </p>
-    <p>
-      Ac ante justo, vitae non arcu. At pretium, mattis porta aliquet malesuada aliquam etiam dignissim. Cras enim
-      auctor sit faucibus id consectetur. Dictum sem sit dignissim vitae. Nulla ornare consequat arcu lacus mauris
-      quis ornare. Ut quam nisi, amet faucibus cursus blandit placerat scelerisque. Mauris sed in pharetra pretium.
-      Sit at sed non ac tellus ligula egestas quam mattis. Morbi sociis mi tincidunt feugiat pharetra facilisis
-      habitant. Eget dictumst est lorem eu neque elit pellentesque mi quam. Erat ut cursus vitae eget. Purus nisi
-      rhoncus, tortor urna tortor. Nibh sed eu, sagittis dapibus diam tristique fermentum adipiscing. A eu tristique
-      et, aliquam tempus, pharetra, scelerisque massa. Sapien, etiam mattis sem nisl aliquet ipsum molestie purus.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro consequatur perferendis ratione illo dolorem
-      ducimus mollitia commodi, enim maiores temporibus necessitatibus a, at esse repudiandae architecto aliquid
-      voluptatum quos. Sequi, perferendis! Neque recusandae, corrupti fugiat quaerat saepe nemo quo consequatur in
-      doloribus blanditiis. Cupiditate suscipit labore quaerat, quis consequuntur eum velit nostrum fuga blanditiis?
-      Aliquam harum illo corporis unde dignissimos, laudantium doloremque nisi reiciendis officiis rerum odit. Quod
-      accusamus natus dolor vitae commodi ipsum, nostrum architecto temporibus a distinctio? Aliquam laboriosam
-      similique nobis accusantium unde aperiam in nostrum sint repudiandae qui tempora aliquid magni odio voluptas
-      maiores, debitis odit placeat autem, nihil dolor sequi saepe sed voluptatem. Libero sit autem quo alias odio
-      consectetur aspernatur provident perferendis, eum atque excepturi similique impedit non ratione veniam sequi,
-      amet quod officia eaque eligendi sed cupiditate quasi maiores accusantium. Nobis corrupti beatae eligendi. Quae
-      nostrum magnam cumque molestias officia numquam iure sequi. Qui repellendus iusto unde voluptatum. Delectus
-      dolorem autem, fugiat obcaecati deserunt eveniet consequatur dolore, ipsam, magnam incidunt praesentium! Magni
-      ipsam earum nesciunt inventore autem illum, amet, excepturi tenetur perferendis aliquid aperiam corporis
-      pariatur labore assumenda qui, aut ullam cupiditate facilis numquam est. Officiis hic sequi ducimus aut
-      voluptate quis facilis consequuntur! Itaque quae esse atque rem commodi dolor nobis aperiam enim suscipit quod
-      minima, sit ea. Mollitia harum exercitationem id quas quibusdam eius dignissimos rerum inventore laboriosam
-      dolores, ut optio numquam nulla veritatis labore aliquam facere eaque suscipit molestias! Dolorum fugiat iure
-      numquam! Assumenda, dolore nobis exercitationem quas fugit eum quibusdam hic neque nesciunt, optio unde nostrum
-      porro quisquam impedit est quia voluptate? Atque voluptatem repellat veritatis hic itaque, in maxime iusto
-      tempora minima numquam nostrum laudantium animi nisi aliquam nam temporibus accusamus laboriosam accusantium
-      doloremque quibusdam dolore aliquid! Harum atque est adipisci laudantium! Praesentium, sapiente delectus!
-      Debitis ratione provident architecto excepturi ipsa aperiam illo sequi aliquam. Temporibus doloremque illum
-      doloribus nam nihil explicabo rerum reprehenderit ullam distinctio aliquid quas, corrupti id qui ipsa totam
-      obcaecati aut beatae at ducimus recusandae consequuntur? Vero similique totam repellat, quo quae iusto quidem
-      sunt architecto distinctio rerum commodi dolor laboriosam vel molestias itaque accusantium perferendis. Hic
-      soluta, quibusdam natus praesentium voluptate ut minus cum laudantium dolores vero corrupti eligendi deleniti
-      nulla autem facere assumenda nihil consectetur ad? Quia repudiandae vero debitis porro modi sequi non tempora
-      laudantium placeat iure doloremque deserunt saepe, dolore dignissimos aperiam, repellendus sit vel? Amet
-      expedita, nihil fugiat suscipit sed illo nulla, ea quas laborum nostrum debitis quisquam dolor non saepe!
-      Incidunt aliquid quia nam reiciendis, itaque libero eum vitae cumque nisi distinctio nulla rem error iure et
-      dicta non magnam maiores sint accusantium quas molestias? Ipsam consequatur ipsum maiores sapiente reiciendis
-      eveniet. Enim, temporibus! Vitae, veritatis fugit ad itaque sed non a quam suscipit excepturi consequatur
-      facilis ipsam, incidunt fuga impedit repudiandae molestias accusantium eaque quos. Provident facere eaque
-      accusantium quod labore ab laboriosam dolorum ullam adipisci sint! Incidunt voluptatem consequatur dolores
-      quaerat consectetur laborum ratione. Delectus veniam quos facilis maiores ab recusandae optio harum laudantium
-      facere iste et nostrum ducimus, atque ea eaque tempora dolorem, blanditiis omnis eos beatae aliquid debitis eum,
-      laborum neque. Eius iste odio laborum ratione distinctio dolor explicabo at vitae adipisci tempora, aperiam
-      aliquid soluta dolorem eligendi! Praesentium deleniti aspernatur itaque expedita nihil. Beatae officiis vero qui
-      ad ea! Voluptatem, aliquam rem. Natus error dolores dignissimos ab doloribus eum non iste ullam, inventore qui
-      dolorem assumenda dolore libero neque eaque iusto ratione officia in reiciendis animi. Recusandae quas maxime
-      neque nesciunt accusamus iusto placeat, doloremque tenetur tempore? Nam sit reiciendis iste! Consectetur magni
-      necessitatibus maxime doloremque vel fugit! Amet harum quae dignissimos molestiae reiciendis consectetur beatae
-      excepturi nihil quos veritatis, unde fugiat quo minus placeat tenetur repellat accusamus officiis veniam,
-      voluptatem deleniti at impedit, iure iusto? Nesciunt necessitatibus alias harum enim, repellendus odit
-      laudantium ipsa fuga, delectus mollitia dolorum a soluta temporibus ipsum rerum. Veritatis nemo quas nesciunt
-      ipsam error molestiae nisi cum consequatur veniam praesentium, accusamus voluptas ea debitis officiis harum
-      tempore at nostrum nobis ullam assumenda quidem voluptatum corporis? Provident odit culpa praesentium impedit
-      consequatur, ipsa tempora ea assumenda reiciendis perspiciatis! Corrupti nulla dicta voluptatibus impedit sed
-      nemo officiis, repudiandae quo similique, at corporis suscipit quidem, nostrum quasi assumenda facere ad
-      ducimus? Eaque illo dolores fugit non nam nostrum possimus vel excepturi blanditiis eos in animi culpa saepe sed
-      a natus laborum distinctio consectetur officia veniam adipisci, inventore atque. Nisi inventore quaerat
-      pariatur, quam, dolorem accusantium deserunt ullam veniam perspiciatis assumenda dolores odio perferendis
-      dolorum minima totam repellat dicta sequi recusandae. Eum ratione quod explicabo unde magnam non culpa voluptate
-      iure modi earum cum, officia doloremque harum amet dolorum hic deleniti porro quos libero sed beatae voluptates
-      soluta odio ipsa. Nesciunt corrupti modi perspiciatis temporibus nobis delectus magni alias porro repellat autem
-      enim illo corporis quia, doloribus quasi incidunt optio omnis vero. Tempore cumque porro beatae consequatur
-      atque id quia error necessitatibus? Optio expedita eaque illo consequatur culpa pariatur, quaerat excepturi
-      doloremque corrupti sapiente. Voluptatibus vel earum in sunt laudantium aliquam nulla quae qui numquam?
-      Inventore vero quisquam tempora praesentium laboriosam exercitationem debitis corrupti iste similique, quae
-      dolorem repudiandae iure eum ratione deserunt maiores aliquam quos, quis dolore minima nostrum adipisci odio!
-      Sequi quibusdam hic iusto aliquam! Amet earum, minima incidunt quos non cum magnam veniam! Aut minima laudantium
-      facere accusamus ex recusandae sit repudiandae itaque corrupti dolor ullam animi iure illo officiis dolores
-      totam ad, veniam modi libero enim nemo. Iste tempora sit numquam molestias laudantium exercitationem cupiditate
-      dignissimos tempore fugiat expedita autem esse consequatur, voluptatum praesentium pariatur quas impedit
-      reiciendis accusantium? Error quasi quisquam reiciendis est, nisi nulla. Rerum commodi mollitia dolore voluptate
-      saepe culpa, quibusdam in consequatur beatae magni atque temporibus suscipit. Magni corrupti, quod dolorum et
-      tempora accusamus reprehenderit voluptatum corporis veniam obcaecati laborum cum voluptas facere optio dicta
-      ipsam quibusdam vel voluptatem. Veritatis, possimus. Est odio placeat fuga. Consequuntur nam fugiat est impedit
-      magni id sequi, doloremque veniam dolorum ea? Porro ratione iure obcaecati, tempore optio nihil maiores
-      voluptatibus autem dicta saepe, suscipit dolorem ut temporibus. Fuga!
-    </p>
 
-<p align="right"> (XXX words) </p>
+<p>The counterintuitive and convoluted nature of the theory of special
+relativity has given rise to several famous paradoxes. Many of these
+have been logically resolved and therefore might technically not be
+considered true paradoxes. However, they are still very useful thought
+experiments to help one to wrap their head around the intricacies of
+special relativity, little brain teasers if you will, so here are some
+of the more prominent ones.</p>
+
+<h2> Twin Paradox </h2>
+
+<p>Suppose there are two identical twins, and one travels on a rocket
+far away into space near the speed of light. When that twin returns, he
+finds that his sibling has aged more due to the effects of time
+dilation. However, since both twins see each other as moving away from
+them, shouldn’t the effects of time dilation apply to both of them,
+leading to both of them finding that the other has aged more?</p>
+<p><img
+src="@/assets/images/021e43e78a7d7b5b9f0d609d6b548e0c018b9693.png"
+style="width:5.96875in;height:3.82292in"
+alt="THE TWINS PARADOX | Cool Knowledge" /></p>
+
+<p>Yes, in actuality, this is a known consequence of Special Relativity.
+Due to the differences in frames between the two twins, there has to be
+a difference in how fast time travels between the two twins, and if
+there is a significant difference in speed between the two observers,
+then they must have aged at different paces.</p>
+
+<p>In fact, this is noticeable in real-life with non-negligible figures.
+Meet Russian cosmonaut Sergei Krikalev.</p>
+
+<p><img
+src="@/assets/images/6394638ff37ca970b72f795c25a74c513136ab5e.jpg"
+style="width:3.01042in;height:3.82292in"
+alt="ESA - Sergei Konstantinovich Krikalev" /></p>
+
+<p>Given the fact he has spent a mind-boggling 803 days in space,
+specifically in orbit around the earth, he has experienced a speed
+faster than all of us. This means, that the effects of time dilation
+apply and he has indeed ‘time-travelled’ approximately 0.02 seconds into
+the future. In other words, he has lived less than everyone else on the
+planet for about 0.02 seconds.</p>
+
+<h2>Ladder paradox a.k.a Barn-Pole Paradox</h2>
+<p>Imagine if you will, a large pole say the length of 2u, and a barn
+with 2 doors, with the length of 1.6u, as in the figure below.</p>
+<p><img
+src="@/assets/images/7fae72385309a9fa9bb77b7010180e3a8e1bf399.png"
+style="width:5in;height:2.25in" /></p>
+<p>It is obvious that the pole could not possibly fit into the barn,
+right? Due to it simply being longer than the barn, the pole will stick
+out on one of the two ends.</p>
+<p>What if, now the pole was moving through the barn. At
+non-relativistic speeds, this would still hold true; as the pole travels
+through the barn, part of it would be visible throughout. Now, let’s
+think about what if the pole was moving through the barn at relativistic
+speeds. Length contraction now becomes a significant consideration. (As
+a refresher, length contraction is the phenomenon of where the length of
+a moving object as measured by one frame of reference is different than
+that measured in its own rest frame).</p>
+<p>As a result, from the inertial frame of the barn, the pole is moving
+at relativistic speeds and hence is therefore shorter and can thus fit
+into the barn comfortably.</p>
+<p><img
+src="@/assets/images/c8318f5db944e151b40c7726d46a962cb8a1cad7.png"
+style="width:5in;height:3.75in" /></p>
+<p>However, from the inertial frame of the pole, the barn is moving at
+relativistic speeds, and hence contracts, making it even less able to
+fit into the barn.</p>
+<p><img
+src="@/assets/images/cef8ade4125d1d52cc2bed26aeeef250274fd7b1.png"
+style="width:5in;height:3.75in" /></p>
+<p>How can it be that the ladder fits within the barn to one observer
+but not the other?</p>
+<p>This may seem like a cursory glance that there is a paradox, yet
+there is an easy explanation to all of this. It lies in the concept of
+the relativity of simultaneity. What <em>exactly</em> do we mean by “the
+pole fitting inside of the barn”? Let us define it as that the right
+side of the pole is to the left of the right door of the barn, what we
+will call condition 1 and the left side of the pole is to the right of
+the left door of the barn, what we will call condition 2, at the same
+time, I.e. simultaneously.</p>
+<p>This is partially also because time is relative to each frame of
+reference.</p>
+<p>In the reference frame of the barn, the two conditions occur at the
+same time, whereas in the reference frame of the pole, condition 1
+happens first in the absence of condition 2, then neither condition are
+fulfilled, afterwards condition 2 occurs in the absence of condition 1.
+(This can also be seen later using a Minkowski diagram, but we will have
+to wait for the video to see that later).</p>
+<h2>Terell Rotation</h2>
+<p>Imagine one is travelling at a significant speed with respect to
+light and observes a stationary cube.</p>
+<p>Now, one would expect that due to the Lorentz Transformations as
+elaborated above, there would be a contraction of the cube along the
+axis which one is travelling.</p>
+
+<a title="Stigmatella aurantiaca, CC BY-SA 4.0 &lt;https://creativecommons.org/licenses/by-sa/4.0&gt;, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:Animated_Terrell_Rotation_-_Cube.gif"><img width="512" alt="Animated Terrell Rotation - Cube" src="https://upload.wikimedia.org/wikipedia/commons/d/d4/Animated_Terrell_Rotation_-_Cube.gif"></a>
+
+<p>However, due to differences in the perception of the back of cubes
+and the front of the cubes, there is indeed an apparent rotation that
+one may notice whilst observing the cube.</p>
+<p>Let us try to explain this phenomenon. The following image is of the
+motion of the observer and of the cube from above.</p>
+<p><img
+src="@/assets/images/216307f38f3200c68c404dfd29388528d809749b.gif"
+style="width:3.11458in;height:2.60417in" /></p>
+<p>Now, one would expect that the light from the back of the cube, in
+this case D and C would be emitted first, than the light from the front
+of the cube, at A and B. Specifically, it would have to have travelled a
+further distance <em>l,</em> the length of a side of the cube, than the
+light coming from A or B.</p>
+<p><img
+src="@/assets/images/5e37e082fc14f0828f385417e733df98087c2f9d.gif"
+style="width:3.20833in;height:2.86458in" /></p>
+<p>Thus, when applying relativity, the observer would have
+simultaneously seen the earlier image of the back side of the cube, D’,
+and a later image of the front side of the cube A. This causes it to
+appear to have rotated horizontally.</p>
+<p>We must remember that according to Einstein’s first postulate that
+the laws of physics must be identical in all inertial frames, hence this
+Terrell rotation must be a purely visual effect – and has no real world
+implications on the state of the cube itself.</p>
+<p>This is the famous rotation that but Terell and Penrose had both
+independently proposed in 1959. An interesting fact of this is that it
+taken a long time for physicists to realise that such an effect indeed
+took place, even 45 years Einstein had proposed Special Relativity.</p>
+<p>This shows us that we should always try to fact-check our
+presumptions in certain contexts, as we may always find new and
+interesting phenomena just lurking beneath physical theories.</p>
+<p align="right"> (1052 words) </p>
     <NextChapter prev-chapter="applications" next-chapter="quiz" />
   </div>
 </template>
