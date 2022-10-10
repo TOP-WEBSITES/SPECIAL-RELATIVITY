fe0e1fa9ac14e118080cc90b81672a54c479166a
mkofdwu <mkofdwu@gmail.com>
2022-10-11 00:03:39 +0800

    update video link

--- src/views/chapters/VideoChapter.vue
+++ src/views/chapters/VideoChapter.vue
@@ -2,9 +2,7 @@
   <div class='chapter-container'>
     <h1>Video</h1>
     <p>Now, watch this video that covers some additional information from the article content</p>
-    <video width="800" controls>
-      <source src="@/assets/video/dave.mp4" type="video/mp4">
-    </video>
+    <iframe width="800" height="500" src="https://www.youtube.com/embed/nXLWFuMyJkw"></iframe>
     <NextChapter prev-chapter="paradoxes" next-chapter="quiz" />
   </div>
 </template>
